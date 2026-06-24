649f96ef2670039187d6b8c326b700d781db14df
Nikhil <nikhilverma2918@gmail.com>
2026-06-24 15:28:39 +0530

    configuration

--- server.js
+++ server.js
@@ -3,31 +3,21 @@ const express = require("express");
 const app = express();
 
 const employeeRoutes = require("./routes/employeeRoutes");
-
 const loggerMiddleware = require("./middleware/loggerMiddleware");
 
-
 // Middleware
-
 app.use(express.json());
-
 app.use(loggerMiddleware);
 
-
 // Routes
-
 app.use("/employees", employeeRoutes);
 
-
 app.get("/", (req, res) => {
-
   res.send("Employee Management API Running");
-
 });
 
+const PORT = process.env.PORT || 5000;
 
-app.listen(5000, () => {
-
-  console.log("Server Running on Port 5000");
-
+app.listen(PORT, () => {
+  console.log(`Server Running on Port ${PORT}`);
 });
