64d459fb9ff5d989ade956b95ccf378cce831894
Nikhil <nikhilverma2918@gmail.com>
2026-06-25 13:36:31 +0530

    Allow Vercel frontend CORS

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ const loggerMiddleware = require("./middleware/loggerMiddleware");
 
 // Middleware
 app.use(express.json());
+app.use((req, res, next) => {
+  const allowedOrigin = process.env.FRONTEND_URL || "*";
+
+  res.setHeader("Access-Control-Allow-Origin", allowedOrigin);
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 app.use(loggerMiddleware);
 
 // Routes
